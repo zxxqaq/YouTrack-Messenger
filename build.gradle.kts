0ceda49a9d6e70a570f02a272b807a63fd9dbaff
Xian Zhang <jp2020213374@qmul.ac.uk>
2025-09-29 19:49:23 +0200

    add dependency for env

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
     implementation("org.slf4j:slf4j-simple:2.0.13")
+    implementation("io.github.cdimascio:dotenv-java:3.0.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
